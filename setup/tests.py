f2ef5b0759eb34ca69a505f9d95adeda4522803b
geraldogrj <geraldogrj@gmail.com>
2022-11-16 22:01:33 -0300

    Configurado o chromedriver do selenium

--- setup/tests.py
+++ setup/tests.py
@@ -4,4 +4,7 @@ from selenium import webdriver
 class AnimaisTestCase(LiveServerTestCase):
 
     def setUp(self):
-        pass
+        self.browser = webdriver.Chrome('/Users/geraldogrj/Projetos/tdd_busca_animal/chromedriver')
+
+    def tearDown(self):
+        self.browser.quit()
